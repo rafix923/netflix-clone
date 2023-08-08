8f1eda1f1d73547b2a0c1927172815c089f1f42d
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-08-08 17:55:42 -0300

    style(fix): Utilizar a propriedade `Image` do next para resolver o erro persistente no terminal.
    Além da propriedade `img` afetar o tempo de renderização e deixa o código esteticamente estranho com as marcações de problemas no código.

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,20 +1,13 @@
+import Input from "@/components/input";
+import Image from "next/image";
+
 const Auth = () => {
   return (
-    <div className="w-full h-screen bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+    <div className="relative h-screen w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-fixed bg-auto">
       <div className="bg-black w-full h-full bg-opacity-50">
         <nav className="px-12 py-5">
-          <img
-            src="/images/logo.png"
-            alt="Logo Netflix com letras de cor vermelho"
-            className="h-12"
-          />
+          <Image src="/images/logo.png" alt="" width={200} height={12} />
         </nav>
-        <div className="flex justify-center">
-          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md w-full">
-            <h2 className="text-white text-4xl mb-8 font-semibold">Entrar</h2>
-            <div className="flex flex-col gap-4">{/* Form */}</div>
-          </div>
-        </div>
       </div>
     </div>
   );
