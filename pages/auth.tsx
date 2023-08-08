73577e999f3655606e7efb7058f760a30270b98f
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-08-08 18:45:23 -0300

    feat: Importar o componente `Input` para a página de autenticação.

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -16,6 +16,9 @@ const Auth = () => {
         <div className="flex justify-center">
           <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
             <h2 className="text-white text-4xl mb-8 font-semibold">Entrar</h2>
+            <div>
+              <Input />
+            </div>
           </div>
         </div>
       </div>
