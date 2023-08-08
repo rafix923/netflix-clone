55b76f0939e44c8a98e64a10988a9a07bfb07058
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-08-08 18:11:51 -0300

    style: Adicionar background semi-transparente e escuro para o fundo do formulário de autenticação.

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -6,8 +6,18 @@ const Auth = () => {
     <div className="relative h-screen w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-fixed bg-auto">
       <div className="bg-black w-full h-full bg-opacity-50">
         <nav className="px-12 py-5">
-          <Image src="/images/logo.png" alt="" width={200} height={12} />
+          <Image
+            src="/images/logo.png"
+            alt="Logo da Netflix com letras de cor vermelho"
+            width={200}
+            height={12}
+          />
         </nav>
+        <div className="flex justify-center">
+          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+            <h2 className="text-white text-4xl mb-8 font-semibold">Entrar</h2>
+          </div>
+        </div>
       </div>
     </div>
   );
