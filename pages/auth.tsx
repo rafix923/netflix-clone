a4197da6c6930eebfdcbffbbd42563caed176223
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-08-12 15:40:39 -0300

    style: Ajustar largura e altura dos elementos input e adicionar efeito de fundo opaco apenas para tela maiores.
    Isso deixará a página de autenticação de usuário mais parecida possível com o da Netflix.

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -17,17 +17,17 @@ const Auth = () => {
 
   return (
     <div className="relative h-screen w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-fixed bg-auto">
-      <div className="bg-black w-full h-full bg-opacity-50">
-        <nav className="px-12 py-5">
+      <div className="bg-black h-full w-full  lg:bg-opacity-50">
+        <nav className="px-8 py-4">
           <Image
             src="/images/logo.png"
             alt="Logo da Netflix com letras de cor vermelho"
-            width={200}
+            width={185}
             height={12}
           />
         </nav>
         <div className="flex justify-center">
-          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 rounded-md">
+          <div className="bg-black bg-opacity-70 px-16 py-4 self-center mt-2 rounded-md">
             <h2 className="text-white text-4xl mb-8 font-semibold">
               {variant === "login" ? "Entrar" : "Assine agora"}
             </h2>
