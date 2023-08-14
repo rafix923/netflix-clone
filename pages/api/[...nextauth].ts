76f78c1c3fd8ba742e0f55dba3c24463e0ea025c
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-08-14 17:04:26 -0300

    feat(auth): Adicionar autenticação usando provedor `Credentials`.
    Configuração do provedor Credentials para permitir autenticação baseada em email e senha. Verifica as credenciais do usuário no banco de dados e autoriza o acesso se forem válidas.

---
+++ pages/api/[...nextauth].ts
@@ -0,0 +1,48 @@
+import NextAuth from "next-auth/next";
+import Credentials from "next-auth/providers/credentials";
+import prismadb from "@/lib/prismadb";
+import { compare } from "bcrypt";
+
+export default NextAuth({
+  providers: [
+    Credentials({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+        },
+        password: {
+          label: "Senha",
+          type: "password",
+        },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error(
+            "Por favor, verifique se o email e a senha estão corretos."
+          );
+        }
+        const user = await prismadb.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+        if (!user || !user.hashedPassword) {
+          throw new Error("Digite o email cadastrado");
+        }
+        const isCorrectPassword = await compare(
+          credentials.password,
+          user.hashedPassword
+        );
+        if (!isCorrectPassword) {
+          throw new Error(
+            "Senha incorreta. Por favor, digite a senha correta."
+          );
+        }
+        return user;
+      },
+    }),
+  ],
+});
