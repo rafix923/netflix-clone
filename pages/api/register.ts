c87ab542ef71e85e6f6ae2e27eeb82762cec0acd
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-08-16 19:34:47 -0300

    feat: Adicionar lógica para registro de novo usuário.

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -19,13 +19,23 @@ export default async function handler(
     });
 
     if (existingUser) {
-      return res
-        .status(422)
-        .json({
-          error:
-            "Por favor, registre outro email. Este email já foi registrado.",
-        });
+      return res.status(422).json({
+        error: "Por favor, registre outro email. Este email já foi registrado.",
+      });
     }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const user = await prismadb.user.create({
+      data: {
+        email,
+        name,
+        hashedPassword,
+        image: "",
+        emailVerified: new Date(),
+      },
+    });
+    return res.status(200).json(user);
   } catch (error) {
     console.log(error);
     return res.status(400).end();
