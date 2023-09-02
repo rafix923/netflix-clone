be00d0441c83e01c5b8d7eabf0411673e9fa69c5
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-09-02 10:53:49 -0300

    style: Aplicar backgound da página `index.tsx` e criar botão de logout.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
+import { signOut } from "next-auth/react";
+
 export default function Home() {
   return (
     <>
-      <h1 className="text-2xl text-green-500">Hello World!</h1>
+      <div className="bg-black h-screen w-screen text-red-600">
+        <h1>Hello World!</h1>
+        <button className="bg-red-600 text-black h-10 w-60">Sair</button>
+      </div>
     </>
   );
 }
