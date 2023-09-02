7022bf1f6df86834561076559fc791d9ff568abe
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-09-02 11:05:09 -0300

    feat: Verificar se o usuário está autenticado.
    Verifica se o usuário está autenticado usando o getSession do NextAuth.js.
    Se o usuário não estiver autenticado, ele é redirecionado para a página de autenticação ("/auth").
    Se o usuário estiver autenticado, a página é renderizada com um objeto de propriedades vazio.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,21 @@
-import { signOut } from "next-auth/react";
+import { NextPageContext } from "next";
+import { getSession, signOut } from "next-auth/react";
 
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {},
+  };
+}
 export default function Home() {
   return (
     <>
