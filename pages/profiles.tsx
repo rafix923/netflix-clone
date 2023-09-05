5e2647a57c14d9a575d45a02a2f79ec7cf3bd26c
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-09-05 14:29:12 -0300

    feat: Criar `Profile page`.

--- pages/profiles.tsx
+++ pages/profiles.tsx
@@ -1,5 +1,6 @@
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
+import Image from "next/image";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -19,9 +20,40 @@ export async function getServerSideProps(context: NextPageContext) {
 
 const Profiles = () => {
   return (
-    <>
-      <h1 className="text-white text-3xl">Profiles Page</h1>
-    </>
+    <div className=" flex items-center h-full justify-center">
+      <div className="flex flex-col">
+        <h1 className="text-white text-3xl md:text-6xl text-center">
+          Quem está assistindo?
+        </h1>
+        <div className="flex items-center justify-center gap-8 mt-10">
+          <div onClick={() => {}}>
+            <div className="group flex-row w-44 mx-auto">
+              <div
+                className="
+              flex items-center 
+              justify-center 
+              h-44 
+              w-44 
+              rounded-md 
+              border-2 
+              border-transparent 
+              group-hover:cursor-pointer 
+              group-hover:border-white 
+              overflow-hidden"
+              >
+                <Image
+                  src="/images/default-blue.png"
+                  alt="Logo da Netflix com letras de cor vermelha"
+                  width={300}
+                  height={300}
+                  className="text-white"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
