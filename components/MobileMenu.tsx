448dc65ab57db84e03a2658217530b35890f4cb8
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-09-05 19:39:34 -0300

    feat: Criar lista de navegação do menu mobile.

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -5,11 +5,20 @@ const MobileMenu: React.FC<MobileMenuProps> = ({ visible }) => {
   if (!visible) return null;
   return (
     <div className="bg-black w-56 absolute top-8 left-0 py-5 flex-col border-2 border-gray-800 fle">
-      <div className="flex flex-col gap-4">
-        <div className="px-3 text-center text-white hover:underline">
-          Inicio
-        </div>
-      </div>
+      <ul className="flex flex-col gap-4">
+        <li className="px-3 text-center text-white hover:underline">Inicio</li>
+        <li className="px-3 text-center text-white hover:underline">Séries</li>
+        <li className="px-3 text-center text-white hover:underline">Filmes</li>
+        <li className="px-3 text-center text-white hover:underline">
+          Bombando
+        </li>
+        <li className="px-3 text-center text-white hover:underline">
+          Minha lista
+        </li>
+        <li className="px-3 text-center text-white hover:underline">
+          Navegar por idiomas
+        </li>
+      </ul>
     </div>
   );
 };
