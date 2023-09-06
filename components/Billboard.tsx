c9d4d889bca8782f339b148bb00e391758015184
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-09-06 09:22:00 -0300

    feat: Adicionar componente `Billboard` para exibição de vídeo dinâmico.
    Este commit introduz o componente Billboard, que utiliza o hook useBillboard para buscar dados dinâmicos da API e exibir um vídeo em um elemento <video>. O vídeo é configurado com autoPlay, loop e som desativado (muted), proporcionando uma experiência de reprodução contínua.

---
+++ components/Billboard.tsx
@@ -0,0 +1,25 @@
+import useBillboard from "@/hooks/useBillboard";
+import React from "react";
+
+const Billboard = () => {
+  const { data } = useBillboard();
+  return (
+    <div className="relative h-[56.25vw]">
+      <video
+        className="
+      w-full
+      h-[56.25vw]
+      object-cover
+      brightness-[60%]
+      "
+        autoPlay
+        muted
+        loop
+        poster={data?.thumbnailUrl}
+        src={data?.videoUrl}
+      ></video>
+    </div>
+  );
+};
+
+export default Billboard;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Billboard from "@/components/Billboard";
 import Navbar from "@/components/Navbar";
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
@@ -21,6 +22,7 @@ export default function Home() {
   return (
     <>
       <Navbar />
+      <Billboard />
     </>
   );
 }
