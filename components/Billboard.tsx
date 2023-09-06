deb7c34993c36b8a9eb46a7c1b353edd4a891bd3
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-09-06 09:59:52 -0300

    feat: Adicionar ao componente Billboard o título, descrição e botão "Mais informações" do filme.

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -1,5 +1,6 @@
 import useBillboard from "@/hooks/useBillboard";
 import React from "react";
+import { AiOutlineInfoCircle } from "react-icons/ai";
 
 const Billboard = () => {
   const { data } = useBillboard();
@@ -18,6 +19,59 @@ const Billboard = () => {
         poster={data?.thumbnailUrl}
         src={data?.videoUrl}
       ></video>
+      <div className="absolute top-[30%] md:top-[40%] ml-4 md:ml-16">
+        <h1
+          className="
+        text-white 
+        text-1xl 
+        md:text-5xl 
+        h-full w-[50%] 
+        lg:text-6xl 
+        font-bold 
+        drop-shadow-xl
+        "
+        >
+          {data?.title}
+        </h1>
+        <p
+          className="
+           text-white 
+           text-[8px]
+           md:text-lg 
+           mt-3
+           md:mt-8
+           w-[90%]
+           md:w-[80%]
+           lg:w-[50%]
+           drop-shadow-xl
+        "
+        >
+          {data?.description}
+        </p>
+        <div className="flex flex-row items-center mt-3 md:mt-4 gap-3">
+          <button
+            className="
+          bg-white
+          text-white
+          bg-opacity-30
+          rounded-md
+          py-1 md:py:2
+          px-2 md:px-4
+          w-auto
+          text-xs lg:text-lg
+          font-semibold
+          flex
+          flex-row
+          items-center
+          hover:bg-opacity-20
+          transition
+          "
+          >
+            <AiOutlineInfoCircle className="mr-1" />
+            Mais informações
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
