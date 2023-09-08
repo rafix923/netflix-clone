a31c117e0350b1c4afd8a45e9526d1c749dde8a8
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-09-08 09:27:17 -0300

    feat: Adiciona ao componente `MovieList` para exibir lista de filmes favoritos.
    Este commit, integra o componente MovieList com a interface de usuário para exibir uma lista de filmes favoritos com o título "Minha lista". Os dados dos filmes favoritos são passados como propriedade data para o componente `MovieList`.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Billboard from "@/components/Billboard";
 import MovieList from "@/components/MovieList";
 import Navbar from "@/components/Navbar";
+import useFavorites from "@/hooks/useFavorites";
 import useMovieList from "@/hooks/useMovieList";
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
@@ -22,12 +23,14 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 export default function Home() {
   const { data: movies = [] } = useMovieList();
+  const { data: favorites = [] } = useFavorites();
   return (
     <>
       <Navbar />
       <Billboard />
       <div className="pb-40">
         <MovieList title="Em alta" data={movies} />
+        <MovieList title="Minha lista" data={favorites} />
       </div>
     </>
   );
