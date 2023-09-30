6c1b01492d34423079db20565ac1e93666b07412
Rafael Ramos de Oliveira Lopes <rafaellopes.dev@gmail.com>
2023-09-30 10:37:20 -0300

    fix(refactor): Alterar a forma que o usuário é direcionado para o perfil após fazer o login com email e senha.
    Resolveu o problema de não encontrar o usuário cadastrado após tentar fazer um novo login com o emai e senha do perfil criado.

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,18 +1,36 @@
-import Input from "@/components/input";
 import axios from "axios";
-import { signIn } from "next-auth/react";
-import Image from "next/image";
+import Input from "@/components/input";
 import { useCallback, useState } from "react";
+import { NextPageContext } from "next";
+import { getSession, signIn } from "next-auth/react";
+import { useRouter } from "next/router";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
 
 const Auth = () => {
-  const [userIdent, setUserIdent] = useState("");
-  const [password, setPassword] = useState("");
-  const [variant, setVariant] = useState("login");
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
+  const router = useRouter();
 
-  // Função para alternar entre os modos de "login" e "register"
-  // A função utiliza o hook useCallback para melhorar a performance e evitar recriações desnecessárias da função durante as renderizações.
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [variant, setVariant] = useState("login");
+
   const toggleVariant = useCallback(() => {
     setVariant((currentVariant) =>
       currentVariant === "login" ? "register" : "login"
@@ -24,12 +42,15 @@ const Auth = () => {
       await signIn("credentials", {
         email,
         password,
-        callbackUrl: "/profiles",
+        redirect: false,
+        callbackUrl: "/",
       });
+
+      router.push("/profiles");
     } catch (error) {
       console.log(error);
     }
-  }, [email, password]);
+  }, [email, password, router]);
 
   const register = useCallback(async () => {
     try {
@@ -38,6 +59,7 @@ const Auth = () => {
         name,
         password,
       });
+
       login();
     } catch (error) {
       console.log(error);
@@ -45,118 +67,65 @@ const Auth = () => {
   }, [email, name, password, login]);
 
   return (
-    <div className="relative h-screen w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-fixed bg-auto">
-      <div className="bg-black h-full w-full  lg:bg-opacity-50">
-        <nav className="px-8 py-4">
-          <Image
-            src="/images/logo.png"
-            alt="Logo da Netflix com letras de cor vermelho"
-            width={185}
-            height={12}
-          />
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+      <div className="bg-black w-full h-full lg:bg-opacity-50">
+        <nav className="px-12 py-5">
+          <img src="/images/logo.png" className="h-12" alt="Logo" />
         </nav>
         <div className="flex justify-center">
-          <div className="bg-black bg-opacity-70 px-16 py-4 self-center mt-2 rounded-md">
+          <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
             <h2 className="text-white text-4xl mb-8 font-semibold">
-              {variant === "login" ? "Entrar" : "Assine agora"}
+              {variant === "login" ? "Sign in" : "Register"}
             </h2>
             <div className="flex flex-col gap-4">
               {variant === "register" && (
                 <Input
-                  label="Nome do usuário"
                   id="name"
-                  onChange={(event: any) => setName(event.target.value)}
+                  type="text"
+                  label="Username"
                   value={name}
+                  onChange={(e: any) => setName(e.target.value)}
                 />
               )}
-              {variant !== "login" && (
-                <>
-                  <Input
-                    label="Email"
-                    id="email"
-                    onChange={(event: any) => setEmail(event.target.value)}
-                    value={email}
-                  />
-                  <Input
-                    label="Senha"
-                    id="password"
-                    onChange={(event: any) => setPassword(event.target.value)}
-                    value={password}
-                  />
-                </>
-              )}
-              {variant === "login" && (
-                <>
-                  <Input
-                    label="Email ou número de telefone"
-                    id="userIdent"
-                    onChange={(event: any) => setUserIdent(event.target.value)}
-                    value={userIdent}
-                  />
-                  <Input
-                    label="Senha"
-                    id="password"
-                    onChange={(event: any) => setPassword(event.target.value)}
-                    value={password}
-                  />
-                </>
-              )}
+              <Input
+                id="email"
+                type="email"
+                label="Email address or phone number"
+                value={email}
+                onChange={(e: any) => setEmail(e.target.value)}
+              />
+              <Input
+                type="password"
+                id="password"
+                label="Password"
+                value={password}
+                onChange={(e: any) => setPassword(e.target.value)}
+              />
             </div>
             <button
-              style={{ backgroundColor: "rgb(229, 9, 20)" }}
-              className="w-full py-3 rounded-md mt-10 text-white font-semibold text-lg"
-              type="submit"
               onClick={variant === "login" ? login : register}
+              className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
             >
-              {variant === "login" ? "Entrar" : "Criar Conta"}
+              {variant === "login" ? "Login" : "Sign up"}
             </button>
-            <div className="flex items-center">
-              <input
-                type="checkbox"
-                id="checkbox"
-                className="mr-2 my-4 rounded bg-red-500"
-              />
-              <div className="flex gap-24">
-                <label
-                  htmlFor="checkbox"
-                  style={{ color: "rgb(156, 153, 153)" }}
-                >
-                  Lembre-se de mim
-                </label>
-                <p style={{ color: "rgb(156, 153, 153)" }}>Precisa de ajuda?</p>
-              </div>
-            </div>
-            <div className="mt-12">
-              <p
-                className="text-lg font-semibold"
-                style={{ color: "rgb(115, 115, 115)" }}
+            <div className="flex flex-row items-center gap-4 mt-8 justify-center"></div>
+            <p className="text-neutral-500 mt-12">
+              {variant === "login"
+                ? "First time using Netflix?"
+                : "Already have an account?"}
+              <span
+                onClick={toggleVariant}
+                className="text-white ml-1 hover:underline cursor-pointer"
               >
-                Novo por aqui?
-                <span
-                  onClick={toggleVariant}
-                  className="text-white ml-1 text-xl font-normal"
-                >
-                  <a href="#" className="hover:underline">
-                    Assine agora
-                  </a>
-                </span>
-                .
-              </p>
-              <p
-                style={{ color: "rgb(115, 115, 115)" }}
-                className=" mt-2.5 leading-tight pb-32"
-              >
-                Esta página é protegida pelo Google reCAPTCHA
-                <br /> para garantir que você não é um robô.
-                <a href="#" className="ml-1 text-blue-500 hover:underline">
-                  Saiba mais.
-                </a>
-              </p>
-            </div>
+                {variant === "login" ? "Create an account" : "Login"}
+              </span>
+              .
+            </p>
           </div>
         </div>
       </div>
     </div>
   );
 };
+
 export default Auth;
